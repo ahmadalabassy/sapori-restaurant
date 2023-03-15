9e6e315b2aaff5de03a901cdc8a17fca34123ba6
Ahmad Alabassy <ahmadalabassy@gmail.com>
2023-03-15 03:02:41 +0100

    Finished Gatsby Tutorial Part 6

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,21 @@
-import * as React from 'react'
-import Layout from '../components/layout'
+import * as React from "react";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+import { StaticImage } from "gatsby-plugin-image";
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Tomatensauce mit frischem Basilikum"
+        src="../images/518103175_c_o.jpg"
+      />
     </Layout>
-  )
-}
+  );
+};
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title="Home Page" />;
 
-export default IndexPage
+export default IndexPage;
